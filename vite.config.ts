5c78911f25c7aa0889d1257845c96622eb3c1af6
Bakary <var.bsan@gmail.com>
2025-03-24 12:02:27 +0100

    implement latest vue starter template; implement antfu eslint config; implement vitest

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,13 @@ import { fileURLToPath, URL } from 'node:url'
 import ui from '@nuxt/ui/vite'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    vueDevTools(),
     ui({
       ui: {
         colors: {
