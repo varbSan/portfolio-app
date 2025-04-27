d727062195351bc7264cc1d2e82b4ff9e54420e3
Bakary <var.bsan@gmail.com>
2025-04-27 09:50:23 +0200

    chore

--- src/pages/PgProjects.vue
+++ src/pages/PgProjects.vue
@@ -43,7 +43,7 @@ const projects = computed<Project[]>(() => [
   {
     name: 'quote-record',
     title: 'Quote Record',
-    description: t('A cross-platform app for daily inspiration. Get a quote/mantra with an AI-generated matching background each day. Insert your own custom quotes and mantras database.'),
+    description: t('A cross-platform app for daily inspiration. Get a quote with an AI-generated matching background each day. Insert your own custom quotes database.'),
     imageSrc: 'https://as1.ftcdn.net/v2/jpg/02/33/17/50/1000_F_233175040_hwqRyiZlQkXimeLz2AIZhajyfiU9El1m.jpg',
     link: 'https://quote-record.bakary.dev',
     techStack: techStack.filter((tech) => {
